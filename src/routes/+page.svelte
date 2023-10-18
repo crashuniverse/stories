8842ae6e324778d8cf52cd789e4a6333ea27f9b2
Priya Ranjan Singh <mail@priyaranjan.net>
2023-10-18 14:17:39 +0530

    add a new story about daisy

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>stories</h1>
+<ul>
+  <li>
+    <a href="/daisy">Daisy</a>
+    <div>Once upon a time in a big, green field, there lived a cute and innocent cow named Daisy.
+      Daisy was white and brown with big, kind eyes. She spent her days ...</div>
+  </li>
+</ul>
---
+++ src/routes/daisy/+page.svelte
@@ -0,0 +1,58 @@
+<script>
+  import cow from './images/1-cow.jpg';
+  import cat from './images/2-cat.jpg';
+  import talk from './images/3-talk.jpg';
+  import walk from './images/4-walk.png';
+  import town from './images/5-town.jpg';
+  import homesick from './images/6-homesick.jpg';
+  import cowReturn from './images/7-return.jpg';
+  import home from './images/8-home.jpg';
+  import bye from './images/9-bye.jpg';
+</script>
+<h1>Daisy</h1>
+<img src={cow} alt="daisy cow" width="100%" />
+<p>Once upon a time in a big, green field, there lived a cute and innocent cow named Daisy.
+  Daisy was white and brown with big, kind eyes. She spent her days munching on sweet,
+  green grass and dreaming of adventures.</p>
+<img src={cat} alt="whispers cat" width="100%" />
+<p>One sunny morning, as Daisy was happily chewing her cud, she heard a soft meow. She looked
+  around and saw a clever cat sitting on the fence nearby. The cat was gray with shiny,
+  green eyes and a mischievous smile.</p>
+<img src={talk} alt="whispers cat" width="100%" />
+<p>"Hello there," said the cat with a friendly purr. "I'm Whiskers. What's your name?"</p>
+<p>Daisy blinked her big, kind eyes and replied, "I'm Daisy. Nice to meet you, Whiskers."</p>
+<p>Whiskers hopped down from the fence and padded over to Daisy. "Daisy," Whiskers said
+  with a sly grin, "I have an idea. How about we go on a road trip together? It will be
+  so much fun!"</p>
+<p>Daisy's eyes sparkled with excitement. She had always wanted to go on an adventure.
+  "Oh, that sounds wonderful, Whiskers! Let's do it!"</p>
+<img src={walk} alt="whispers cat" width="100%" />
+<p>So, Daisy and Whiskers set off on their road trip. They walked down dusty roads, crossed
+  bubbling brooks, and even climbed a little hill. Along the way, they met friendly birds,
+  chatty squirrels, and even a colorful butterfly.</p>
+<img src={town} alt="whispers cat" width="100%" />
+<p>As the sun began to set, Daisy and Whiskers found themselves in a cozy little town.
+  The town had colorful houses, twinkling lights, and the sweet scent of freshly baked
+  cookies filled the air.</p>
+<p>Daisy and Whiskers decided to stop and rest for the night. They found a friendly farmer
+  who offered them a warm barn to sleep in. Inside the barn, Daisy and Whiskers snuggled
+  together and talked about all the amazing things they had seen on their adventure.</p>
+<img src={homesick} alt="whispers cat" width="100%" />
+<p>But as the night grew darker, Daisy started to feel a little homesick. She missed her
+  green field and her friends, the other cows. She whispered to Whiskers, "I miss home,
+  Whiskers. I want to go back."</p>
+<p>Whiskers understood how Daisy felt. He knew he had to help his new friend. "Don't worry,
+  Daisy," Whiskers said softly. "I'll help you find your way back home tomorrow."</p>
+<img src={cowReturn} alt="whispers cat" width="100%" />
+<p>Early the next morning, Whiskers woke up and guided Daisy back to her big, green field.
+  Daisy's heart was filled with joy as she saw her familiar surroundings and her cow friends.</p>
+<img src={home} alt="whispers cat" width="100%" />
+<p>Daisy turned to Whiskers and said, "Thank you, Whiskers, for the wonderful adventure.
+  You're a clever and kind friend."</p>
+<p>Whiskers smiled and replied, "You're welcome, Daisy. I'm glad I could help you. If you
+  ever want to go on another adventure, just let me know."</p>
+<img src={bye} alt="whispers cat" width="100%" />
+<p>And so, Daisy and Whiskers said their goodbyes, knowing that they would always be
+  friends, even if they lived in different worlds. Daisy went back to her happy life
+  in the big, green field, and Whiskers continued his clever adventures, knowing that
+  true friends are always there when you need them.</p>
